2783f383223598964fd131b579eb957914586f25
schlucht <web@jagolo.ch>
2025-03-07 12:02:45 +0000

    DELETE Project

--- public/resources/views/home/index.blade.php
+++
@@ -1,9 +0,0 @@
-<x-home-layout>
-    <x-nav-link>
-        Hallo
-    </x-nav-link>
-    <h1 class="text-3xl text-white">Page Layout</h1>
-    <x-card>
-        <p class="text-white">Hier kommt Card Text</p>
-    </x-card>
-</x-home-layout>
